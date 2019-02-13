f45205e7f86c1684198c84ea57f320a7f57a1b47
andrejmalykhin <Andrej.Malykhin.13@gmail.com>
2019-02-13 22:13:04 +0200

    Initial commit

---
+++ src/test/java/com/giggs13/springbootthymeleafdemo/SpringbootThymeleafDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.giggs13.springbootthymeleafdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootThymeleafDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
